ab44d78ca8c9f4b04e33f9d5a1d5821af88e0cc2
mestrevh <victor-torugo@hotmail.com>
2025-06-21 23:30:02 -0300

    feat: create the schemma on database for user

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,28 +1,29 @@
 import { Injectable } from '@nestjs/common';
-import { User } from './interfaces/user.interface';
+import { UserEntity } from './interfaces/user.entity';
 import { CreateUserDto } from './dtos/createUser.dto';
 import { hash } from 'bcrypt';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
 
 @Injectable()
 export class UserService {
-    private users: User[] = [];
 
-    async createUser(createUserDto: CreateUserDto): Promise<User> {
+    constructor (   @InjectRepository(UserEntity)
+                    private readonly userRepository: Repository <UserEntity>,
+                ){}
+
+    async createUser(createUserDto: CreateUserDto): Promise<UserEntity> {
         const saltOrRounds = 10;
         const passwordHash = await hash(createUserDto.password, saltOrRounds);
 
-        const user: User = {
+
+        return this.userRepository.save({
             ...createUserDto,
-            id: this.users.length + 1,
-            password: passwordHash
-        }
-
-        this.users.push(user)
-
-        return user;
+            password: passwordHash,
+        })
     }
 
-    async getAllUsers(): Promise <User[]> {
-        return this.users;
+    async getAllUsers(): Promise <UserEntity[]> {
+        return this.userRepository.find();
     }
 }
